d3ef834e395cbf661b343314cc23ef4a01bd8b09
revinindiaokt <154623249+revinindiaokt@users.noreply.github.com>
2024-05-05 08:39:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// menu toogle
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+  nav.classList.toggle('slide');
+})
